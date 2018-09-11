2866d8c3ea76bed25febc62fbdd1b0d5de98781c
jessica kanmegne <jessicatcheyanou@gmail.com>
2018-09-11 16:49:37 +0200

    model and home page modified

---
+++ src/model/user.model.ts
@@ -0,0 +1,9 @@
+export interface UserModel {
+    key?: string;
+    email: string;
+    password: string;
+    matricule: string;
+    status: string ;
+    level: string ;
+    fullname:string;
+}
